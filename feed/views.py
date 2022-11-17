1cdd5a91ea0b4e71cbef96151681a7a427ba5900
abbappii <bappi142434@gmail.com>
2022-11-17 19:20:30 +0600

    friendlist and friend request  model logics

--- feed/views.py
+++ feed/views.py
@@ -7,10 +7,12 @@ from .models import Feed, Post, Comment, Like
 from .serializers import PostSerializers, CommentSerializers
 
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 class PostCreateViewset(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         if hasattr(self.request.user,'feed_author'):
@@ -20,3 +22,4 @@ class PostCreateViewset(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets
         else:
             raise serializer.ValidationError("feed not matched.")
 
+ 
--- friend/models.py
+++ friend/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from feed.initials import Initials
 
-# Create your models here.
+class FriendList(models.Model):
+    user = models.OneToOneField('account.User',on_delete=models.CASCADE, related_name='friend_list_user')
+    friends = models.ManyToManyField('account.User',blank = True, related_name='friends')
+
+    def __str__(self):
+        return self.user.username
+    
+    def add_friend(self,account):
+        "add friend"
+    
+        if not account in self.friends.all():
+            self.friends.add(account)
+            self.save()
+
+    def remove_friend(self,account):
+        if account in self.friends.all():
+            self.friends.remove(account)
+
+    def unfriend(self,removee):
+        remover_friends_list = self
+        remover_friends_list.remove(removee)
+
+        # remove friend from removee friend list 
+        friend_list = FriendList.objects.get(user=removee)
+        friend_list.remove_friend(self.user)
+
+
+
+class FriendRequests(Initials):
+    # person who receive request 
+    # and who send request
+
+    sender = models.ForeignKey('account.User',on_delete=models.CASCADE,related_name='sender')
+    receiver = models.ForeignKey('account.User',on_delete=models.CASCADE,related_name='receiver')
+
+    def __str__(self):
+        return self.sender.username
+
+    def accept(self):
+        receiver_friend_list = FriendList.objects.get(user=self.receiver)
+        if receiver_friend_list:
+            receiver_friend_list.add_friend(self.sender)
+
+            sender_friend_list = FriendList.objects.get(user=self.sender)
+            if sender_friend_list:
+                sender_friend_list.add_friend(self.receiver)
+                self.is_active  = False
+                self.save()
+
+    def decline(self):
+        self.is_active = False
+        self.save()
+
+    def cancel(self):
+        self.is_active = False
+        self.save()
+
+
+
+
+
