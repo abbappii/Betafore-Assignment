b07dea1d347bfde81a6914abe919130b26924f19
abbappii <bappi142434@gmail.com>
2022-11-17 10:52:50 +0600

    jwt token added

--- Betafore_assignment/urls.py
+++ Betafore_assignment/urls.py
@@ -1,4 +1,4 @@
-"""Betafore_assignment URL Configuration
+"""asset_management URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # simple-jwt(webtoken)
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
