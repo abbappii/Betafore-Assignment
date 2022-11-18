92614ecdbc986bfb2bd224743ac4f21d9ade7451
abbappii <bappi142434@gmail.com>
2022-11-18 15:42:27 +0600

    post on my feed and friends post on my feed done

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,11 @@
 
-from django.urls import path 
+from django.urls import path , include
+from .views import PostCreateViewset
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('timeline-post',PostCreateViewset,basename='post')
 
 urlpatterns = [ 
-    
+    path("",include(router.urls)),
 ]
